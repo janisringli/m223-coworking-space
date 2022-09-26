4bb1d869ecfb6af10fd871d5f59c3ab1aec96ac5
Janis Ringli <janis.ringli@gmail.com>
2022-09-26 12:50:55 +0200

    feat(UserController): add GET POST PUT DELETE methode for userController

--- src/main/java/ch/zli/m223/controller/UserController.java
+++ src/main/java/ch/zli/m223/controller/UserController.java
@@ -1,5 +1,53 @@
 package ch.zli.m223.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import ch.zli.m223.model.User;
+import ch.zli.m223.service.UserService;
+
+@Path("/user")
+@Tag(name = "User", description = "Handling of User")
 public class UserController {
-    
+
+    @Inject
+    UserService userService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Index all Users.", description = "Returns a list of all users.")
+    public List<User> index() {
+        return UserService.findAll();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Creates a new user.", description = "Creates a new user and returns the newly added user.")
+    public User create(User user) {
+       return userService.createTags(user);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void delete(long id) {
+       userService.deleteUser(id);
+     }
+
+     @PUT
+      public void update(User user){
+         userService.update(user);
+     }
 }
