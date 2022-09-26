09105cd78520c3b5ee7d2738b4f35be939027729
Janis Ringli <janis.ringli@gmail.com>
2022-09-26 13:16:40 +0200

    feat(userService): add userService

--- src/main/java/ch/zli/m223/service/UserService.java
+++ src/main/java/ch/zli/m223/service/UserService.java
@@ -1,5 +1,39 @@
 package ch.zli.m223.service;
 
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+
+import ch.zli.m223.model.User;
+
+@ApplicationScoped
 public class UserService {
-    
+    @Inject
+    private EntityManager entityManager;
+
+    @Transactional
+    public User createTags(User user) {
+        entityManager.persist(user);
+        return user;
+    }
+
+    @Transactional
+    public void deleteTags(long id) {
+        User user =  entityManager.find(User.class, id);
+        entityManager.remove(user);
+    }
+
+    @Transactional
+    public void update(User user){
+        entityManager.merge(user);
+    }
+
+    public List<User> findAll() {
+        var query = entityManager.createQuery("FROM User", User.class);
+        return query.getResultList();
+    }
 }
