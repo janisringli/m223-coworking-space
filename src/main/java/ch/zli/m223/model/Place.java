0f118f3f616993d0114f96c5d9ddc82de593a657
Janis Ringli <janis.ringli@gmail.com>
2022-09-26 11:28:03 +0200

    feat(Place): add Place Model including getters and setters

--- src/main/java/ch/zli/m223/model/Place.java
+++ src/main/java/ch/zli/m223/model/Place.java
@@ -1,5 +1,31 @@
 package ch.zli.m223.model;
 
+
+import javax.persistence.*;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Entity
 public class Place {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column(nullable = false)
+  private Long placeNumber;
+
+  public Long getId(){
+    return id;
+  }
+  public Long getPlaceNumber(){
+    return placeNumber;
+  }
+
+  public void setId(Long id){
+    this.id = id;
+  }
+  public void setPlaceNumber(Long placeNumber){
+    this.placeNumber = placeNumber;
+  }
 }
