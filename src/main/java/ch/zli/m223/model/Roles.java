70e32c4334e0d25d4d86454f7fe7edda6a0bf262
Janis Ringli <janis.ringli@gmail.com>
2022-09-26 11:21:45 +0200

    feat(Roles): add Roles Model including getters and setters

--- src/main/java/ch/zli/m223/model/Roles.java
+++ src/main/java/ch/zli/m223/model/Roles.java
@@ -1,5 +1,36 @@
 package ch.zli.m223.model;
 
+
+import javax.persistence.*;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+
+@Entity
 public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column(nullable = false)
+  private String roleName;
+
+
+  
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getRoleName() {
+    return roleName;
+  }
+
+  public void setRoleName(String roleName){
+    this.roleName = roleName;
+  }
     
 }
