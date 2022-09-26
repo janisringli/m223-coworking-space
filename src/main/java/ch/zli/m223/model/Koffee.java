cabe3ec7a89dae63fde77c8de4b8d4c189c145f2
Janis Ringli <janis.ringli@gmail.com>
2022-09-26 11:35:08 +0200

    feat(Koffee): add Koffee Model including getters and setters

--- src/main/java/ch/zli/m223/model/Koffee.java
+++ src/main/java/ch/zli/m223/model/Koffee.java
@@ -1,5 +1,42 @@
 package ch.zli.m223.model;
 
+import java.security.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Entity
 public class Koffee {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column(nullable = false)
+  private Float price;
+
+  @Column(nullable = false)
+  private LocalDateTime time;
+
+  public Long getId(){
+    return id;
+  }
+  public void setId(Long id){
+    this.id = id;
+  }
+  public Float getPrice(){
+    return price;
+  }
+  public void setPrice(Float price){
+    this.price = price;
+  }
+  public LocalDateTime getTime(){
+    return time;
+  }
+  public void setTime(LocalDateTime time){
+    this.time = time;
+  }
 }
