821932f94ac08dd858d93d4332511161ac8f0b19
Janis Ringli <janis.ringli@gmail.com>
2022-09-27 13:39:42 +0200

    feat(PlaceController): add all GET, PUT, PULL, DELETE function

--- src/main/java/ch/zli/m223/controller/PlaceController.java
+++ src/main/java/ch/zli/m223/controller/PlaceController.java
@@ -1,5 +1,64 @@
 package ch.zli.m223.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import ch.zli.m223.model.Place;
+import ch.zli.m223.service.PlaceService;
+
+@Path("/place")
+@Tag(name = "place", description = "Handling of place")
 public class PlaceController {
-    
+
+    @Inject
+    PlaceService placeService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Index all Places.", description = "Returns a list of all Places.")
+    public List<Place> indexById() {
+        return placeService.findAll();
+    }
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Index one Place.", description = "Returns a list of one place.")
+    @Path("/{id}")
+    public List<Place> index(Long id) {
+        return List.of(placeService.getPlaceById(id));
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Creates a new user.", description = "Creates a new user and returns the newly added user.")
+    public Place create(Place place) {
+       return placeService.createPlace(place);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void delete(long id) {
+    placeService.deletePlace(id);
+    }
+
+     @PUT
+     @Path("/{id}")
+      public void update(Long id, Place place) {
+        place.setId(id);
+        placeService.update(place);
+     }
 }
